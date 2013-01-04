427c2548c7402cc7442a9e94bc5540e9e0c7c9ba
Pirkka Jokela <pirkka.jokela@gmail.com>
2013-01-04 18:54:27 +0200

    Fixes to RSS to make it valid.

--- LapsMobileWeb/src/my/laps/mobileweb/RssXml.scala
+++ LapsMobileWeb/src/my/laps/mobileweb/RssXml.scala
@@ -3,29 +3,36 @@ package my.laps.mobileweb
 import scala.xml.Elem
 import my.laps.mobile.TrackStatus
 import my.laps.mobile.TrackPracticeDay
+import scala.xml.Attribute
+import scala.xml.Text
+import scala.xml.Null
 
 class RssXml(val conf : UserConf) {
   def trackActivityRss(day : TrackPracticeDay) : Elem = {
     val track = day.track
-    val items = for(session <- day.sessionsNewestFirst.reverse) yield {
+    val items = for(session <- day.sessionsNewestFirst.take(10)) yield {
         <item>
           <title>{conf.formatDate(session.date) + " " + session.driver.name + " (" + session.driver.transponder.number + ")"}</title>
           <link>http://m-laps.appspot.com/app?tid={track.tid}&amp;transponder={session.driver.transponder.number}</link>
           <description>
-            <h2>{session.driver.name}</h2>
-            <p>Practice session at {track.name} during {conf.formatDate(session.date)}. Total of {session.passings} passings.</p>
+            {session.driver.name}
+            Practice session at {track.name} during {conf.formatDate(session.date)}. Total of {session.passings} passings.
           </description>
-          <guid>trackActivityRss_{session.date.getTime}_{session.driver.transponder.number}</guid>
+          <guid isPermaLink="false">trackActivityRss_{session.date.getTime}_{session.driver.transponder.number}</guid>
         </item>
     }
     
-    <rss version="2.0">
+    val atomLink = <atom:link href="http://m-laps.appspot.com/rss?tid={track.tid}" rel="self" type="application/rss+xml" /> % 
+      Attribute(None, "href", Text("http://m-laps.appspot.com/rss?tid=" + track.tid), Null)
+    
+    return <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
       <channel>
         <title>{track.name} Practice Sessions</title>
         <link>http://m-laps.appspot.com/app?tid={track.tid}</link>
         <description>This feed lists all practice sessions from {track.name} at 
         {track.location}. Please see website for more details about the sessions.
         </description>
+        {atomLink}
         {items}
       </channel>
     </rss> 
